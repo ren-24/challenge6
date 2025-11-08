ef067fa4c8dcf0943c65cf2fa4f948118babe980
ren <251205128@ccmailg.meijo-u.ac.jp>
2025-11-08 17:12:07 +0900

    課題１実装

--- src/data/posts.js
+++ src/data/posts.js
@@ -4,7 +4,7 @@ export const posts = [
     id: 1,
     title: '記事タイトル１',
     thumbnailUrl: 'https://placehold.jp/800x400.png',
-    createdAt: '2023-09-11T09:00:00.000Z',
+    createdAt: '2023/9/11',
     categories: ['React', 'TypeScript'],
     content: `
     本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
@@ -13,7 +13,7 @@ export const posts = [
     id: 2,
     title: '記事タイトル２',
     thumbnailUrl: 'https://placehold.jp/800x400.png',
-    createdAt: '2023-09-10T09:00:00.000Z',
+    createdAt: '2023/09/10',
     categories: ['HTML', 'CSS'],
     content: `
     本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
@@ -22,7 +22,7 @@ export const posts = [
     id: 3,
     title: '記事タイトル３',
     thumbnailUrl: 'https://placehold.jp/800x400.png',
-    createdAt: '2023-09-09T09:00:00.000Z',
+    createdAt: '2023/9/9',
     categories: ['JavaScript'],
     content: `
     本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
