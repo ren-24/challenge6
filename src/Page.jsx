a1f8e9e3fe7d99a8e529a0359a1f3a0f7cce405a
ren <251205128@ccmailg.meijo-u.ac.jp>
2025-11-17 15:22:03 +0900

    課題2修正(3)

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,6 +1,6 @@
+import React from "react";
 import { posts } from "./data/posts";
 import classes from "./Home.module.css";
-import React from "react";
 import { useParams } from "react-router-dom";
 
 export default function Page() {
@@ -8,9 +8,13 @@ export default function Page() {
   const { id } = useParams();
   const elem = posts.find(post => post.id === Number(id));
 
+  if (!elem) {
+    return <div>記事が見つかりません</div>;
+  }
+
    return (
     <React.Fragment key={elem.id}>
-      <li>
+      <div>
         <img className={classes.img} src={elem.thumbnailUrl} alt={elem.title} />
         <div className={classes.flex}>
           <div className={classes.date}>{elem.createdAt}</div>
@@ -20,12 +24,12 @@ export default function Page() {
             ))}
           </div>
         </div>
-        <p className={classes.title}>APIで取得した{elem.title}</p>
-        <p
+        <div className={classes.title}>APIで取得した{elem.title}</div>
+        <div
           className={classes.content}
           dangerouslySetInnerHTML={{ __html: elem.content }}
-        ></p>
-      </li>
+        ></div>
+      </div>
     </React.Fragment>
   );
 }
